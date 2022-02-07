36122f160793a04ec0fb1d2cc047451be0e8eb57
Nhzzha <85264632+Nhzzha@users.noreply.github.com>
2022-02-07 08:52:41 +0800

    Update Surgerunningmode.js

--- Surgerunningmode.js
+++ Surgerunningmode.js
@@ -27,7 +27,7 @@ let config = {
   silence: false, // 是否静默运行，默认false
   cellular: "RULE", // 蜂窝数据下的模式，RULE代表规则模式，PROXY代表全局代理，DIRECT代表全局直连
   wifi: "RULE", // wifi下默认的模式
-  all_direct: ["Redmi_DC8A_5G", "Redmi_DC8A"], // 指定全局直连的wifi名字
+  all_direct: ["Redmi_DC8A_5G", "Redmi_DC8A", "M1nhzzha5G"], // 指定全局直连的wifi名字
   all_proxy: [], // 指定全局代理的wifi名字
 };
 
